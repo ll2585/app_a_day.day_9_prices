3d7a62e10fde6aa124e503a5a73d1519cd7e655b
Luke Li <luke.li@nera.com>
2015-03-04 14:41:54 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/lukeli/appaday/day9_prices/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.lukeli.appaday.day9_prices;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
